b1788472976b20110d8890c926cee98f2a9cb021
Shailesh <shlshkrk@gmail.com>
2020-09-03 10:23:46 +1000

    added pdf download to roster

--- roster/src/Components/Roster/WeeklyRoster.js
+++ roster/src/Components/Roster/WeeklyRoster.js
@@ -2,10 +2,16 @@ import React, { useEffect, useState } from "react";
 import useApi from "../../hooks/useApi";
 import LeftSidebar from "../LeftSidebar";
 import { MDBContainer, MDBRow, MDBCol, MDBTable } from "mdbreact";
-
+import ReactToPdf from "react-to-pdf";
 import { SplitButton, Dropdown, ButtonGroup, Button } from "react-bootstrap";
-
 import "../StaffListScreen.css";
+
+const ref = React.createRef();
+const options = {
+  orientation: "potrait",
+  unit: "in",
+  format: [6000, 1400],
+};
 
 function WeeklyRoster(props) {
   const [totalHour, setTotalHour] = useState(0);
@@ -58,81 +64,100 @@ function WeeklyRoster(props) {
         <MDBCol size="12" sm="12" md="12" lg="12" xl="12">
           <MDBRow>
             <MDBCol>
-              <MDBTable style={{ wordBreak: "break-all" }} bordered hover>
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    {tableHeader.map((rosterDay) => (
-                      <th>{rosterDay}</th>
-                    ))}
-                    {/* <th>Total</th> */}
-                    <th>Total Hours</th>
-                  </tr>
-                </thead>
-                {/* <tbody>{tableBody}</tbody> */}
-                <tbody>
-                  {Object.keys(shifts).map((staff) => (
-                    <tr>
-                      <th>{staff}</th>
-                      {tableHeader.map((rosterDay) => (
-                        <td>
-                          {Object.values(shifts).map((shift_) =>
-                            shift_.map((shift, index, arr) =>
-                              staff === shift.username &&
-                              rosterDay == shift.work_date ? (
-                                shift.start_time === "" &&
-                                rosterDay == shift.work_date ? (
-                                  ""
-                                ) : (
-                                  <p
-                                    onLoad={() =>
-                                      (totalHour +=
-                                        totalHour + shift.shift_duration)
-                                    }
-                                  >
-                                    {shift.work_date}
-                                    <br />
-                                    {shift.start_time}
-                                    {" To "} {shift.end_time}
-                                    <br />
-                                    {shift.break_length}
-                                    {" Hour Break"}
-                                    <br />
-                                    {shift.shift_duration}
-                                    {" Hours"}
-                                  </p>
+              <div style={{ width: 100 + "vw" }}>
+                <ReactToPdf
+                  targetRef={ref}
+                  filename="roster.pdf"
+                  options={options}
+                  x={1}
+                  y={0.1}
+                  scale={1.5}
+                >
+                  {({ toPdf }) => <Button onClick={toPdf}>Download pdf</Button>}
+                </ReactToPdf>
+                <div style={{ width: 100 + "vw" }} ref={ref}>
+                  <MDBTable
+                    sm="12"
+                    style={{ wordBreak: "break-all" }}
+                    bordered
+                    hover
+                  >
+                    <thead>
+                      <tr>
+                        <th>Name</th>
+                        {tableHeader.map((rosterDay) => (
+                          <th>{rosterDay}</th>
+                        ))}
+                        {/* <th>Total</th> */}
+                        <th>Total Hours</th>
+                      </tr>
+                    </thead>
+                    {/* <tbody>{tableBody}</tbody> */}
+                    <tbody>
+                      {Object.keys(shifts).map((staff) => (
+                        <tr>
+                          <th>{staff}</th>
+                          {tableHeader.map((rosterDay) => (
+                            <td>
+                              {Object.values(shifts).map((shift_) =>
+                                shift_.map((shift, index, arr) =>
+                                  staff === shift.username &&
+                                  rosterDay == shift.work_date ? (
+                                    shift.start_time === "" &&
+                                    rosterDay == shift.work_date ? (
+                                      ""
+                                    ) : (
+                                      <p
+                                        onLoad={() =>
+                                          (totalHour +=
+                                            totalHour + shift.shift_duration)
+                                        }
+                                      >
+                                        {shift.work_date}
+                                        <br />
+                                        {shift.start_time}
+                                        {" To "} {shift.end_time}
+                                        <br />
+                                        {shift.break_length}
+                                        {" Hour Break"}
+                                        <br />
+                                        {shift.shift_duration}
+                                        {" Hours"}
+                                      </p>
+                                    )
+                                  ) : (
+                                    ""
+                                  )
                                 )
-                              ) : (
-                                ""
-                              )
-                            )
+                              )}
+                            </td>
+                          ))}
+                          <td>
+                            {shifts[staff].reduce(
+                              (totalHour, shift) =>
+                                totalHour + shift.shift_duration,
+                              0
+                            )}
+                          </td>
+                        </tr>
+                      ))}
+                      <tr>
+                        <td>Total</td>
+                        {Object.keys(Total).map((rosterDate) => (
+                          <td>{Total[rosterDate]}</td>
+                        ))}
+                        <td>
+                          {Object.values(Total).reduce(
+                            (total, individualDayTotal) =>
+                              total + individualDayTotal,
+                            0
                           )}
                         </td>
-                      ))}
-                      <td>
-                        {shifts[staff].reduce(
-                          (totalHour, shift) =>
-                            totalHour + shift.shift_duration,
-                          0
-                        )}
-                      </td>
-                    </tr>
-                  ))}
-                  <tr>
-                    <td>Total</td>
-                    {Object.keys(Total).map((rosterDate) => (
-                      <td>{Total[rosterDate]}</td>
-                    ))}
-                    <td>
-                      {Object.values(Total).reduce(
-                        (total, individualDayTotal) =>
-                          total + individualDayTotal,
-                        0
-                      )}
-                    </td>
-                  </tr>
-                </tbody>
-              </MDBTable>
+                      </tr>
+                    </tbody>
+                  </MDBTable>
+                </div>
+              </div>
             </MDBCol>
           </MDBRow>
         </MDBCol>
