bede596354e5e561c96eb599d7f425e8412d728f
Shailesh <shlshkrk@gmail.com>
2020-09-12 12:00:59 +1000

    updated login.js sql

--- roster-api/db/login.js
+++ roster-api/db/login.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 
 const doLogin = async (email, password) => {
   const sql =
-    "select users.email,users.password, users.user_id, groups.title from users,staff,stafftogroups,groups where users.user_id=staff.user_id and staff.staff_id=stafftogroups.staff_member_id and stafftogroups.staff_role_id=groups.id and users.email=$1;";
+    "select users.email,users.password, users.user_id, groups.title from users,staff,stafftogroups,groups where users.email=staff.email and staff.staff_id=stafftogroups.staff_member_id and stafftogroups.staff_role_id=groups.id and users.email=$1;";
   const params = [email];
   const { rows } = await runSql(sql, params);
   if (bcrypt.compareSync(password, rows[0].password)) {
